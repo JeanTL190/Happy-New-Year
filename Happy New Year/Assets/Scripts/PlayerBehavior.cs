ee665a4f99b24c562ed1e6bf0d2d28b1630bcf1e
Jean Tan Li <chines1998@hotmail.com>
2020-01-27 23:28:19 -0300

    Movimentação aprimorada

--- Happy New Year/Assets/Scripts/PlayerBehavior.cs
+++ Happy New Year/Assets/Scripts/PlayerBehavior.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    Vector3 posiMouse,movMouseAnte,mov;
+    [SerializeField] float x, y;
+    [SerializeField] float time=2f;
+    [SerializeField] Animator anim; 
     void Start()
     {
         Cursor.visible = false;
-
+        StartCoroutine("VerificaPosiMouseAnte");
     }
-
+    IEnumerator VerificaPosiMouseAnte()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(time);
+            movMouseAnte = posiMouse;
+        }
+    }
+    private void FixedUpdate()
+    {
+        mov = posiMouse- movMouseAnte;
+        
+        if(mov.normalized.y>0)
+        {
+            anim.SetInteger("Fly", 1);
+        }
+        else if(mov.normalized.y<0)
+        {
+            anim.SetInteger("Fly", -1);
+        }
+        else
+        {
+            anim.SetInteger("Fly", 0);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition)+Vector3.forward*10;
+        posiMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition) + Vector3.forward * 10;
+
+        if (posiMouse.x > x)
+        {
+            if (posiMouse.y > y)
+                transform.position = new Vector3(x, y);
+            else if(posiMouse.y < -y)
+                transform.position = new Vector3(x, -y);
+            else
+                transform.position = new Vector3(x, posiMouse.y);
+        }
+        else if(posiMouse.x < -x)
+        {
+            if (posiMouse.y > y)
+                transform.position = new Vector3(-x, y);
+            else if (posiMouse.y < -y)
+                transform.position = new Vector3(-x, -y);
+            else
+                transform.position = new Vector3(-x, posiMouse.y);
+        }
+        else
+        {
+            if (posiMouse.y > y)
+                transform.position = new Vector3(posiMouse.x, y);
+            else if (posiMouse.y < -y)
+                transform.position = new Vector3(posiMouse.x, -y);
+            else
+                transform.position = new Vector3(posiMouse.x, posiMouse.y);
+        }
+        
     }
 }
